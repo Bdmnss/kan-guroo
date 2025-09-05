8a7b3839ffedd44a9e189da75261621f614d1d67
Luka Badali <150358418+Bdmnss@users.noreply.github.com>
2025-09-05 22:48:40 +0400

    Add Favorites page

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,5 +1,29 @@
+import { useFavoritesStore } from "../stores/favoritesStore";
+import ProductsGrid from "../components/ProductsGrid";
+import { useEffect, useState } from "react";
+
 const Favorites = () => {
-  return <div className="p-10 text-center text-2xl">Favorites Page</div>;
+  const favorites = useFavoritesStore((state) => state.favorites);
+  const [checked, setChecked] = useState(false);
+
+  useEffect(() => {
+    if (!localStorage.getItem("fake_token")) {
+      window.location.href = "/login?redirect=/favorites";
+    } else {
+      setChecked(true);
+    }
+  }, []);
+
+  if (!checked) return null;
+
+  return (
+    <div className="bg-light flex min-h-screen flex-col items-center py-20 transition-colors duration-500 dark:bg-dark">
+      <h1 className="mb-6 text-5xl font-extrabold text-black drop-shadow-lg transition-colors duration-500 dark:text-white">
+        Favorites
+      </h1>
+      <ProductsGrid products={favorites} />
+    </div>
+  );
 };
 
 export default Favorites;
